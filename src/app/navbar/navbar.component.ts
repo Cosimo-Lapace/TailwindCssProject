c430e0715d24c659b9d8decd66e2ab1b568d99fc
CosimoLP <cosimolapace@libero.it>
2023-06-19 17:24:44 +0200

    darkmode

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -23,4 +23,11 @@ export class NavbarComponent {
       this.opacity?.nativeElement.classList.remove("opacity-0")
     }
   }
+  darkmodeToggle(){
+    if(document.body.classList.contains('dark')){
+      document.body.classList.remove('dark')
+    }else{
+      document.body.classList.add('dark')
+    }
+  }
 }
